a22950eff8c66573ae5512ccfbd61b0bf2e9dbca
AngelinaLagunova <lagunova_gelya@mail.ru>
2024-04-24 14:34:02 +1000

    new file:   .DS_Store 	new file:   buttons.css 	new file:   chansha.html 	new file:   chansha.pug 	new file:   chanshaFood.html 	new file:   chanshaFood.pug 	new file:   flesh.js 	new file:   img/card.png 	new file:   img/chansha.png 	new file:   img/home.png 	new file:   img/lk.png 	new file:   img/map1.png 	new file:   img/mapicon.png 	new file:   img/sound.png 	new file:   img/tofu.jpg 	new file:   img/tofuFront.png 	new file:   index.css 	new file:   index.html 	new file:   index.pug 	new file:   lk.html 	new file:   lk.pug 	new file:   location.css 	new file:   map.css 	new file:   map.html 	new file:   map.pug 	new file:   mixin.pug 	new file:   scroll.js 	new file:   template.css 	new file:   template.pug 	new file:   words.css 	new file:   "\320\277\320\273\320\260\320\275 \321\200\320\260\320\261\320\276\321\202" 	new file:   "\321\201\320\260\320\271\321\202.zip" 	new file:   "\321\201\320\260\320\271\321\202/chansha.html" 	new file:   "\321\201\320\260\320\271\321\202/chansha.png" 	new file:   "\321\201\320\260\320\271\321\202/home.png" 	new file:   "\321\201\320\260\320\271\321\202/index.css" 	new file:   "\321\201\320\260\320\271\321\202/index.html" 	new file:   "\321\201\320\260\320\271\321\202/lk.html" 	new file:   "\321\201\320\260\320\271\321\202/lk.png" 	new file:   "\321\201\320\260\320\271\321\202/location.css" 	new file:   "\321\201\320\260\320\271\321\202/map.css" 	new file:   "\321\201\320\260\320\271\321\202/map.html" 	new file:   "\321\201\320\260\320\271\321\202/map.png" 	new file:   "\321\201\320\260\320\271\321\202/map1.png" 	new file:   "\321\201\320\260\320\271\321\202/mapicon.png" 	new file:   "\321\201\320\260\320\271\321\202/scroll.js" 	new file:   "\321\201\320\260\320\271\321\202/template.css"

---
+++ flesh.js
@@ -0,0 +1,15 @@
+function flesh(id){
+    let spin = document.getElementById(id);
+    let divs = spin.children;
+    if (divs[2].innerHTML=="Вонючий тофу"){
+        divs[1].innerHTML = "<img src='sound.png'>";
+        divs[2].innerHTML = "臭豆腐";
+        divs[3].innerHTML = "chòudòufu";
+    }
+
+    else{
+        divs[1].innerHTML = "";
+        divs[2].innerHTML = "Вонючий тофу";
+        divs[3].innerHTML = "<img src='tofu.jpg'>";
+    }
+}
